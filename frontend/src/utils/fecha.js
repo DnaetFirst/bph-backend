be4d70013a504d2bf4ddd1891d8152c2880dccca
DnaetFirst <tu.email@ejemplo.com>
2026-08-04 21:18:47 -0400

    fix: normalizarNombre now case-insensitive to handle DB encoding mismatch in excluyeAreasJson

--- frontend/src/utils/fecha.js
+++ frontend/src/utils/fecha.js
@@ -7,4 +7,4 @@ export const getLocalISODate = () => {
 };
 
 export const normalizarNombre = (str) =>
-  str.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+  str.normalize('NFD').replace(/[\u0300-\u036f]/g, '').toLowerCase();
